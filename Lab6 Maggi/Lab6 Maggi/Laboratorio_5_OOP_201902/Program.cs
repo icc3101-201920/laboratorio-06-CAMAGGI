f28781bb7daae5b7b381f1198f72f1f48436e7fc
cristobal maggi <camaggi1@miuandes.cl>
2019-09-27 10:18:20 -0400

    Commit 03

--- Lab6 Maggi/Lab6 Maggi/Laboratorio_5_OOP_201902/Program.cs
+++ Lab6 Maggi/Lab6 Maggi/Laboratorio_5_OOP_201902/Program.cs
@@ -18,67 +18,67 @@ namespace Laboratorio_5_OOP_201902
             player.Deck.Shuffle();
             player.FirstHand();
             player.ChooseCaptainCard(game.Captains[0]);
-            //Console.WriteLine($"Player captain card: {player.Captain.Name}\n");
+            Console.WriteLine($"Player captain card: {player.Captain.Name}\n");
             int counter = 1;
-           // Console.WriteLine("Player Hand:");
+            Console.WriteLine("Player Hand:");
             foreach (Card card in player.Hand.Cards)
             {
-             //   Console.WriteLine($"{counter++}: {card.Name}");
+                Console.WriteLine($"{counter++}: {card.Name} {card.Type.ToString()}");
             }
             counter = 1;
-            //Console.WriteLine("\nPlayer Deck:");
-            Visualization.ShowDecks(game.Decks);
+            Console.WriteLine("\nPlayer Deck:");
+            
             foreach (Card card in player.Deck.Cards)
             {
-            //    Console.WriteLine($"{counter++}: {card.Name}");
+                Console.WriteLine($"{counter++}: {card.Name}");
             }
             
             //Test Change Card
             
-            //player.ChangeCard(3);
-            //counter = 1;
-            //Console.WriteLine("\n New Player Hand:");
-            //foreach (Card card in player.Hand.Cards)
-            //{
-            //    Console.WriteLine($"{counter++}: {card.Name}");
-            //}
-            //counter = 1;
-            //Console.WriteLine("\n New Player Deck:");
-            //foreach (Card card in player.Deck.Cards)
-            //{
-            //    Console.WriteLine($"{counter++}: {card.Name}");
-            //}
+            player.ChangeCard(3);
+            counter = 1;
+            Console.WriteLine("\n New Player Hand:");
+            foreach (Card card in player.Hand.Cards)
+            {
+                Console.WriteLine($"{counter++}: {card.Name}");
+            }
+            counter = 1;
+            Console.WriteLine("\n New Player Deck:");
+            foreach (Card card in player.Deck.Cards)
+            {
+                Console.WriteLine($"{counter++}: {card.Name}");
+            }
             
 
             //Test draw card
             
-            //player.DrawCard();
-            //counter = 1;
-            //Console.WriteLine("\n New Player Hand:");
-            //foreach (Card card in player.Hand.Cards)
-            //{
-            //    Console.WriteLine($"{counter++}: {card.Name}");
-            //}
-            //counter = 1;
-            //Console.WriteLine("\n New Player Deck:");
-            //foreach (Card card in player.Deck.Cards)
-            //{
-            //    Console.WriteLine($"{counter++}: {card.Name}");
-            //}
+            player.DrawCard();
+            counter = 1;
+            Console.WriteLine("\n New Player Hand:");
+            foreach (Card card in player.Hand.Cards)
+            {
+                Console.WriteLine($"{counter++}: {card.Name}");
+            }
+            counter = 1;
+            Console.WriteLine("\n New Player Deck:");
+            foreach (Card card in player.Deck.Cards)
+            {
+                Console.WriteLine($"{counter++}: {card.Name}");
+            }
             
 
             //Test play card
             
-            //player.PlayCard(6);
-            //counter = 1;
-            //Console.WriteLine("\n New Player Hand:");
-            //foreach (Card card in player.Hand.Cards)
-            //{
-            //    Console.WriteLine($"{counter++}: {card.Name}");
-            //}
-            //Console.WriteLine(String.Join(", ", board.GetMeleeAttackPoints()));
-            //Console.WriteLine(String.Join(", ", board.GetRangeAttackPoints()));
-            //Console.WriteLine(String.Join(", ", board.GetLongRangeAttackPoints()));
+            player.PlayCard(6);
+            counter = 1;
+            Console.WriteLine("\n New Player Hand:");
+            foreach (Card card in player.Hand.Cards)
+            {
+                Console.WriteLine($"{counter++}: {card.Name}");
+            }
+            Console.WriteLine(String.Join(", ", board.GetMeleeAttackPoints()));
+            Console.WriteLine(String.Join(", ", board.GetRangeAttackPoints()));
+            Console.WriteLine(String.Join(", ", board.GetLongRangeAttackPoints()));
             
             
         }
--- Lab6 Maggi/Lab6 Maggi/Laboratorio_5_OOP_201902/Visualization.cs
+++ Lab6 Maggi/Lab6 Maggi/Laboratorio_5_OOP_201902/Visualization.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Laboratorio_5_OOP_201902.Cards;
+using Laboratorio_5_OOP_201902.Enums;
 
 namespace Laboratorio_5_OOP_201902
 {
@@ -18,10 +19,25 @@ namespace Laboratorio_5_OOP_201902
             Console.WriteLine($"Player Hand:");
             foreach (Card card in hand.Cards)
             {
-                Console.WriteLine($"{c++}: {card.Name}");
+                
+                if  (card.Type == EnumType.melee ||
+                     card.Type == EnumType.range ||
+                     card.Type == EnumType.longRange)
+                    {
+                    CombatCard tempCard =card as CombatCard;
+                    System.Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"||{c++}: {card.Name} ({card.Type.ToString()}): {tempCard.AttackPoints}||");
+                    System.Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    System.Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{c++}: {card.Name} ({card.Type.ToString()})");
+                    System.Console.ForegroundColor = ConsoleColor.White;
+                }
             }
             
-            
+          
         }
 
 
@@ -51,14 +67,18 @@ namespace Laboratorio_5_OOP_201902
 
         }
 
-        static void ConsoleError(string message)
+        public static void ConsoleError(string message)
         {
-
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.BackgroundColor = ConsoleColor.Black;
         }
 
-        static void ShowProgramMessage(string message)
+        public static void ShowProgramMessage(string message)
         {
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         static void ShowListOptions(List<string> options, string message = null)
         {
